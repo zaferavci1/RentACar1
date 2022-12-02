8124e678a63b00389ceefdaff3e0f7279efe20f9
sserkanyilmaz <92798864+sserkanyilmaz@users.noreply.github.com>
2022-12-02 15:51:35 +0300

    serkan

---
+++ RentACar/Application/Admin/Commands/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RentACar.Application.Admin.Commands
+{
+    public class Class1
+    {
+    }
+}
