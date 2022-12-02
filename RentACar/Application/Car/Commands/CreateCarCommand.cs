bc1c9344196212e91f46f8115cdf00fc71e1ac31
Zafer Avcı <zaferavci31@gmail.com>
2022-12-02 22:04:58 +0300

    ilk command mappersiz eklendi

---
+++ RentACar/Application/Car/Commands/CreateCarCommand.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using RentACar.Models;
+using RentACar.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RentACar.Application.Car.Commands
+{
+    public class CreateCarCommand
+    {
+        private readonly IMapper mapper;
+        public CreateCarModel carModel { get; set; }
+        private readonly Context _context;
+
+        public void Handle()
+        {
+            var car = _context.Cars.SingleOrDefault(x => x.Brand == carModel.Brand);
+            if(car != null)
+            {
+                throw new Exception("bu markada bir araba var");
+            }
+
+            _context.Cars.Add(car);
+            _context.SaveChanges();
+        }
+    }
+
+    public class CreateCarModel
+    {
+        public string Brand { get; set; }
+        public Color Color { get; set; }
+        public int Price { get; set; }
+        public int SeatCount { get; set; }
+    }
+}
