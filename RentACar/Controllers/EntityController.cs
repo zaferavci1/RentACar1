7ca23d072d85a46e276bf65be23b3dafb4a14a22
sserkanyilmaz <92798864+sserkanyilmaz@users.noreply.github.com>
2022-12-10 23:17:18 +0300

    Dbyi düzeltmeye çalışıldı

---
+++ RentACar/Controllers/EntityController.cs
@@ -0,0 +1,22 @@
+﻿using RentACar.Application.Car.Queries.GetCars;
+using RentACar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentACar.Controllers
+{
+    public class EntityController : Controller
+    {
+        RentACarDbContext context = new RentACarDbContext();
+        // GET: Entity
+        public ActionResult Index()
+        {
+            GetCarsQuery query = new GetCarsQuery(context);
+            var cars = query.Handle();
+            return View(cars);
+        }
+    }
+}
