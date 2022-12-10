5792e3b2d4dbf598a16d4926c52e17a6a811644b
Zafer Avcı <zaferavci31@gmail.com>
2022-12-10 23:22:07 +0300

    güncelleme

---
+++ RentACar/Migrations/202212091252485_updateTablo.cs
@@ -0,0 +1,16 @@
+﻿namespace RentACar.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updateTablo : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ RentACar/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace RentACar.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<RentACar.Models.RentACarDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(RentACar.Models.RentACarDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
